b1fe43ddd448afd74acd3ef9abe956a2f42297eb
Monivillagra <villagra.ramirez@gmail.com>
2017-01-07 15:44:05 -0300

    se agrega mapa

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,68 +1,25 @@
+// tooltip
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
 });
-
-// function get_regiones(){
-//     return [
-//         {
-//             name: "arica",
-//             distance: 2059
-//         },
-//         {
-//             name: "iquique",
-//             distance: 1789
-//         },
-//         {
-//             name: "antofagasta",
-//             distance: 1368
-//         },
-//         {
-//             name: "copiapo",
-//             distance: 1567
-//         },
-//         {
-//             name: "la_serena",
-//             distance: 470
-//         },
-//         {
-//             name: "valparaiso",
-//             distance: 116
-//         },
-//         {
-//             name: "rancagua",
-//             distance: 84
-//         },
-//         {
-//             name: "talca",
-//             distance: 257
-//         },
-//         {
-//             name: "concepcion",
-//             distance: 500
-//         },
-//         {
-//             name: "temuco",
-//             distance: 690
-//         },
-//         {
-//             name: "valdivia",
-//             distance: 848
-//         },
-//         {
-//             name: "puerto_montt",
-//             distance: 1032
-//         },
-//         {
-//             name: "coyhaique",
-//             distance: 1888
-//         },
-//         {
-//             name: "punta_arenas",
-//             distance: 3004
-//         },
-//         {
-//             name: "santiago",
-//             distance: 0
-//         }
-//     ]
-// }
+// mapa
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: {lat: -33.45, lng:-70.6667}
+  });
+}
+// se habilita el selector de vehiculos
+$(document).ready(function(){
+		$('#origen').change(function(){
+  			$('#destino').removeAttr('disabled');
+		});
+});
+$(document).ready(function(){
+		$('.opcion').change(function(){
+  			$('#btnBuscar').removeAttr('disabled');
+		});
+});
+// mostrar autos al hacer click en boton buscar
+function buscar(){
+	document.getElementById('info-auto').style.display = 'block';}
